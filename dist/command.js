20d003a266a829ff3150eebaedde138f5276fddf
Franciscus Agnew <agnew_f@hotmail.com>
2026-01-15 18:25:05 -0500

    Modified src/command.ts and added src/command_pokedex.ts.

--- dist/command.js
+++ dist/command.js
@@ -4,6 +4,7 @@ import { commandMap, commandMapB } from "./command_map.js";
 import { commandExplore } from './command_explore.js';
 import { commandCatch } from './command_catch.js';
 import { commandInspect } from './command_inspect.js';
+import { commandPokedex } from './command_pokedex.js';
 // export type CLICommand = {
 // 	name: string;
 // 	description: string;
@@ -45,6 +46,11 @@ export function getCommands() {
             name: "inspect <pokemon-name>",
             description: "View details about a Pokemon you have caught.",
             callback: commandInspect,
+        },
+        pokedex: {
+            name: "pokedex",
+            description: "View all Pokemon you have caught.",
+            callback: commandPokedex,
         },
     };
 }
--- src/command.ts
+++ src/command.ts
@@ -5,6 +5,7 @@ import { commandMap, commandMapB } from "./command_map.js";
 import { commandExplore } from './command_explore.js';
 import { commandCatch } from './command_catch.js';
 import { commandInspect } from './command_inspect.js';
+import { commandPokedex } from './command_pokedex.js';
 
 // export type CLICommand = {
 // 	name: string;
@@ -48,6 +49,11 @@ export function getCommands(): Record<string, CLICommand> {
 			name: "inspect <pokemon-name>",
 			description: "View details about a Pokemon you have caught.",
 			callback: commandInspect,
+		},
+		pokedex: {
+			name: "pokedex",
+			description: "View all Pokemon you have caught.",
+			callback: commandPokedex,
 		},
 	};
 }
